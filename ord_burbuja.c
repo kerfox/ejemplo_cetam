e0582f98c8a78475da395be0ce4ffb89926e1b93
esteban <keramik@gmail.com>
2022-05-05 19:38:33 -0500

    Generacion de numeros aleatorios

--- ord_burbuja.c
+++ ord_burbuja.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
+#define SIZE 10
+
+int main(){
+    int ord_vector[SIZE];
+    srand(time(NULL));
+    printf("MAX: %d\n", RAND_MAX);
+    printf("Generación de numeros aleatorios\n");
+    for(int i=0;i<SIZE;i++){
+        ord_vector[i] = 100+rand()%101;
+        printf("%d) %d\n", i, ord_vector[i]);
+    }
+    return 0;
+}
