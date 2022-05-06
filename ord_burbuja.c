13d6f0533c8ecd796f3dec5b62d0e066189d23dd
esteban <keramik@gmail.com>
2022-05-05 19:51:53 -0500

    Numeros ordenados

--- ord_burbuja.c
+++ ord_burbuja.c
@@ -13,5 +13,18 @@ int main(){
         ord_vector[i] = 100+rand()%101;
         printf("%d) %d\n", i, ord_vector[i]);
     }
+    for(int i=0;i<SIZE-1;i++){
+        for(int j=i+1;j<SIZE;j++){
+            if(ord_vector[i]>ord_vector[j]){
+                int k= ord_vector[i];
+                ord_vector[i] = ord_vector[j];
+                ord_vector[j] = k;
+            }
+        }
+    }
+    printf("Vector ordenado:\n");
+    for(int i=0;i<SIZE;i++){
+        printf("%d) %d\n", i, ord_vector[i]);
+    }
     return 0;
 }
